242a1a861378ccd462b57bfbed15014e2f06c6ed
Raj <raj.nair@colorado.edu>
2024-09-17 20:56:38 +0000

    Committing after Replit test

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import e from "express";
+import express from "express";
 import { createClient } from "redis";
-import cors from 'cors';
+import cors from "cors";
 
-const app = e()
-const port = 3001
+const app = express();
+const port = 3001;
 /*
 let corsOptions = {
   origin : ['http://localhost:3000'],
@@ -12,23 +12,20 @@ let corsOptions = {
 
 app.use(cors());
 
-
 //Creating and opening a redis client that'll help fetch the data into the database.
-const client = createClient();  
-client.on('error', err => console.log('Redis Client Error', err));
+const client = createClient();
+client.on("error", (err) => console.log("Redis Client Error", err));
 await client.connect();
 
+app.get("/jobs", async (req, res) => {
+  //Once the API fetches the data, calling the Redis client to collect it.
+  const value = await client.get("jobPostings");
+  console.log(JSON.parse(value).flat().length);
 
-app.get('/jobs', async (req, res) => {
-
-//Once the API fetches the data, calling the Redis client to collect it.
- const value = await client.get('jobPostings'); 
- console.log(JSON.parse(value).flat().length);
-
- res.send(value);
+  res.send(value);
   //res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
